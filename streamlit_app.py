67c858b148b7f6c6a2ebd01b0ad4376197df93c9
Abigail Swan <99372448+ABBYHSWAN@users.noreply.github.com>
2025-10-28 16:32:50 +0000

    Markdown switch

--- streamlit_app.py
+++ streamlit_app.py
@@ -437,8 +437,8 @@ zip_buffer.seek(0)
 st.divider()
 st.subheader("Step 3️⃣: Process & Download") 
 
-st.write("Reminder!: Contact ID fields must be filled after running through Investran.")
-st.write("The \"Linked Contact ID\" in **3 - Investran Investor Upload.csv** must then be populated manually.")
+st.markdown("Reminder!: Contact ID fields must be filled after running through Investran.\n\n"\
+    "The \"Linked Contact ID\" in **3 - Investran Investor Upload.csv** must then be populated manually.")
 
 
 # Streamlit download button for ZIP
