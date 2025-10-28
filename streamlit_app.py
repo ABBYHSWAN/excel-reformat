453f8524abd3df114d3e9e7c67f63c3c476368ff
Abigail Swan <99372448+ABBYHSWAN@users.noreply.github.com>
2025-10-28 14:56:24 +0000

    States mapping

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,7 +219,23 @@ df2["Email Email is Default"] = np.where(df2["Email Email"].notna(), "yes", pd.N
 df2["Business Address Street"] = trans_df["street"]
 df2["Business Address is Default"] = np.where(df2["Business Address Street"].notna(), "yes", pd.NA)
 df2["Business Address City"] = trans_df["city"]
-df2["Business Address State"] = trans_df["state"]
+
+# Assign df2 "Business Address State" using mapping
+state_mapping = {
+    "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "AS": "American Samoa",
+	"CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District of Columbia",
+	"FL": "Florida", "GA": "Georgia","GU": "Guam","HI": "Hawaii","ID": "Idaho","IL": "Illinois",
+	"IN": "Indiana","IA": "Iowa","KS": "Kansas","KY": "Kentucky","LA": "Louisiana","ME": "Maine",
+	"MD": "Maryland","MA": "Massachusetts","MI": "Michigan","MN": "Minnesota","MS": "Mississippi",
+	"MO": "Missouri","MT": "Montana","NE": "Nebraska","NV": "Nevada","NH": "New Hampshire","NJ": "New Jersey",
+	"NM": "New Mexico","NY": "New York","NC": "North Carolina","ND": "North Dakota","MP": "Northern Mariana Islands",
+	"OH": "Ohio","OK": "Oklahoma","OR": "Oregon","PA": "Pennsylvania","PR": "Puerto Rico","RI": "Rhode Island",
+	"SC": "South Carolina","SD": "South Dakota","TN": "Tennessee","TX": "Texas","TT": "Trust Territories",
+	"UT": "Utah","VT": "Vermont","VA": "Virginia","VI": "Virgin Islands","WA": "Washington","WV": "West Virginia",
+	"WI": "Wisconsin","WY": "Wyoming",
+}
+df2["Business Address State"] = trans_df["state"].map(state_mapping).fillna(trans_df["state"])
+
 df2["Business Address Zip/Postal Code"] = trans_df["zip"]
 df2["Home Phone"] = cont_df["contactPhone"]
 df2["Primary Phone"] = np.where(df2["Home Phone"].notna(), "yes", pd.NA)
